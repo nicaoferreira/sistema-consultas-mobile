b062871b6d1e1c101c730f4dc06b110475f6a6d8
Laboratório FIAP <labsfiap@fiap.com.br>
2026-08-13 21:57:16 -0300

    Entrega do nivelamento - Marcação de Consultas

--- src/interfaces/consulta.ts
+++ src/interfaces/consulta.ts
@@ -1,16 +1,16 @@
 import { StatusConsulta, Especialidade } from "../types";
 
 export interface Consulta {
- id: number;
- pacienteId: number;
- pacienteNome: string;
- medicoId: number;
- medicoNome: string;
- especialidade: Especialidade;
- usuarioId: number; // ID do usuário que agendou a consulta
- data: string;
- horario: string;
- status: StatusConsulta;
- observacoes?: string;
- valor?: number;
+  id: number;
+  pacienteId: number;
+  pacienteNome: string;
+  medicoId: number;
+  medicoNome: string;
+  especialidade: Especialidade;
+  usuarioId: number; // ID do usuário que agendou a consulta
+  data: string;
+  horario: string;
+  status: StatusConsulta;
+  observacoes?: string;
+  valor?: number;
 }
